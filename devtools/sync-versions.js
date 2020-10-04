9039c73e288d5e03d45c5e54271afe01dcd95535
Pablo Klaschka <contact@pabloklaschka.de>
2020-10-04 23:23:20 +0200

    Added npm version support

---
+++ devtools/sync-versions.js
@@ -0,0 +1,26 @@
+/**
+ * Syncs versions `package.json` => `manifest.json`
+ * @author Pablo Klaschka
+ */
+
+const fs = require('fs');
+const path = require('path');
+const sh = require('shelljs');
+const ch = require('chalk');
+
+const { version } = require('../package.json');
+
+const manifestPath = path.join(__dirname, '../dist/manifest.json');
+
+const manifest = fs.readFileSync(manifestPath).toString();
+
+const newManifest = manifest.replace(
+	/"version": "\d+\.\d+\.\d+"/g,
+	`"version": "${version}"`
+);
+
+fs.writeFileSync(manifestPath, newManifest);
+
+console.log(ch.green(`Updated manifest version to ${ch.blue(version)}`));
+
+sh.exec(`git add ${manifestPath}`, { silent: true });
