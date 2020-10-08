42a1a4d5113c4dbcce5b33253f0ef1d5db67f96a
Pablo Klaschka <contact@pabloklaschka.de>
2020-10-08 17:03:40 +0200

    Enhanced header tests

--- src/components/general-elements/header/header.cy.tsx
+++ src/components/general-elements/header/header.cy.tsx
@@ -1,13 +1,16 @@
 /// <reference types="cypress" />
 import mount from 'cypress-react-unit-test';
 import React from 'react';
+import { HashRouter } from 'react-router-dom';
 import '../../chat/index.scss';
 import { Header } from './header';
 
 function Parent(props: { children: React.ReactNode }) {
 	return (
 		// <BrowserRouter>
-		<div className="wrapper">{props.children}</div>
+		<HashRouter>
+			<div className="wrapper">{props.children}</div>
+		</HashRouter>
 		// </BrowserRouter>
 	);
 }
@@ -21,5 +24,19 @@ describe('Header', () => {
 		);
 
 		cy.contains('Hello World');
+	});
+
+	it('renders the settings link', () => {
+		mount(
+			<Parent>
+				<Header toLink={'/settings'} title={'Hello World'} />
+			</Parent>
+		);
+		cy.screenshot();
+
+		cy.location().hash().should('be.eql', '#/');
+
+		cy.get('img').click();
+		cy.location().hash().should('be.eql', '#/settings');
 	});
 });
