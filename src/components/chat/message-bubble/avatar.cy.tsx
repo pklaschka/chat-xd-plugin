6e6f927970e23895e9ce00265f694ef5f69e2423
Pablo Klaschka <contact@pabloklaschka.de>
2020-10-15 13:53:04 +0200

    Moved avatar tests to avatar.cy.tsx

--- src/components/chat/message-bubble/avatar.cy.tsx
+++ src/components/chat/message-bubble/avatar.cy.tsx
@@ -3,23 +3,46 @@ import mount from 'cypress-react-unit-test';
 import React from 'react';
 import { Avatar } from './avatar';
 
+const user = {
+	uuid: '',
+	gravatarMail: 'xdplugins@pabloklaschka.de',
+	name: 'John Doe'
+};
+
 describe('Avatar component', () => {
 	it('works', () => {
-		mount(
-			<Avatar
-				gravatar={true}
-				author={{
-					uuid: '',
-					gravatarMail: 'xdplugins@pabloklaschka.de',
-					name: 'fjowe'
-				}}
-			/>
-		);
+		mount(<Avatar gravatar={true} author={user} />);
 		// now use standard Cypress commands
 		cy.get('.Avatar')
 			.should('be.visible')
 			.and((img) => {
 				expect((img[0] as HTMLImageElement).naturalWidth).to.be.greaterThan(0);
 			});
+	});
+
+	it('uses Gravatar avatars with gravatar={true}', () => {
+		mount(<Avatar gravatar={true} author={user} />);
+
+		cy.get('img.Avatar').should('exist');
+		cy.screenshot();
+	});
+
+	describe('placeholder avatar', () => {
+		beforeEach(() => {
+			mount(<Avatar gravatar={false} author={user} />);
+		});
+
+		it('use placeholder avatars with gravatar={false}', () => {
+			cy.get('img.Avatar').should('not.exist');
+			cy.screenshot();
+		});
+
+		it('should use "A" as initial if the auhtor\'s name is empty', () => {
+			mount(
+				<Avatar gravatar={false} author={Object.assign(user, { name: '' })} />
+			);
+
+			cy.get('.Avatar').should('contain', 'A');
+		});
 	});
 });
