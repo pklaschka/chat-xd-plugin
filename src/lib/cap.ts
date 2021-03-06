0d199001f1d506f94de7a30a863c95569220f3a9
Pablo Klaschka <contact@pabloklaschka.de>
2021-03-06 17:08:07 +0100

    docs(lib): Add code documentation to all `lib` functions

--- src/lib/cap.ts
+++ src/lib/cap.ts
@@ -1,14 +1,16 @@
 /**
  * Return a number as close as possible to `val` while being at least `min` at
  * at most `max`
- * @param min minimum
- * @param val target value
- * @param max maximum
  *
- * @return `val`, if it's between `min` and `max`, `min` if `val < min`, and
+ * @param min - minimum
+ * @param val - target value
+ * @param max - maximum
+ *
+ * @returns `val`, if it's between `min` and `max`, `min` if `val < min`, and
  * `max` if `val > max`
  *
- * @example ```
+ * @example
+ * ```ts
  * cap(0, 3, 5) // 3
  * cap(0, -2, 5) // 0
  * cap(0, 7, 5) // 5
--- src/lib/uxp-logger.ts
+++ src/lib/uxp-logger.ts
@@ -5,8 +5,25 @@ const fs = uxp.storage.localFileSystem;
 
 let messages: string[] = [];
 
+/**
+ * A {@link LogFunctionFactory} for a UXP-plugin-specific log function
+ *
+ * @returns A `LogFunction` that, periodically, saves
+ * messages to a file in the plugin's data folder.
+ *
+ * @example
+ * ```ts
+ * import { Logger, PlainLogger, WebhookLogger } from '@fliegwerk/logsemts';
+ * const logger = new Logger({
+ *     loggers: [ UXPLogger() ]
+ * });
+ * ```
+ */
 export const UXPLogger: LogFunctionFactory = () => {
 	return (type, style, componentName, componentStyle, ...args) => {
+		// [1] Push the message to the messages.
+		// In the code below (at [2]), they will get stored into the file
+		// periodically.
 		messages.push(
 			`[ ${new Date().toISOString()} ]\t[ ${type} ]\t[ ${componentName} ]\t${args
 				.map((a) => a.toString())
@@ -15,6 +32,7 @@ export const UXPLogger: LogFunctionFactory = () => {
 	};
 };
 
+// [2] Periodically save the messages from [1] to the file:
 (async () => {
 	const folder = await fs.getDataFolder();
 
