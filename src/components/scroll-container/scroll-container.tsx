d8ea6a6596154a54e5fff830ac859e951a45b62a
Pablo Klaschka <contact@pabloklaschka.de>
2021-03-06 20:24:56 +0100

    refactor(components): Refactor and document scroll container

--- src/components/scroll-container/scroll-container.tsx
+++ src/components/scroll-container/scroll-container.tsx
@@ -1,91 +1,50 @@
-import React, {
-	ReactNode,
-	useCallback,
-	useLayoutEffect,
-	useRef,
-	useState
-} from 'react';
-import cap from '../../lib/cap';
+import React, { ReactNode } from 'react';
 import './scroll-container.scss';
+import { useCustomScrollController } from './use-custom-scroll-controller';
+
+/**
+ * Props for the {@link ScrollContainer} component
+ */
+type ScrollContainerProps = { children: ReactNode };
 
 /**
  * A custom scrolling container optimized for chat windows.
  *
- * Scrolls down when new messages get appended, if it is previously fully scrolled to the bottom.
+ * @param props - the props passed to the component
+ * @returns the rendered {@link JSX.Element}
  *
- * Shows a "scroll to bottom" button, if not fully scrolled to the bottom.
+ * @example
+ * ```tsx
+ * <ScrollContainer>
+ *     [...] (Many children)
+ * </ScrollContainer>
+ * ```
  */
-export default function ScrollContainer({ children }: { children: ReactNode }) {
-	const [x, setX] = useState(0);
-	const [deltaX, setDeltaX] = useState(0);
-	const [oldDeltaH, setOldDeltaH] = useState(0);
-
-	const innerContainer = useRef<HTMLDivElement>(null);
-	const outerContainer = useRef<HTMLDivElement>(null);
-
-	const scrollDelta = useCallback(
-		(deltaDeltaX: number) => {
-			setDeltaX(deltaX + deltaDeltaX);
-		},
-		[x]
-	);
-
-	useLayoutEffect(() => {
-		const deltaH =
-			(innerContainer.current?.scrollHeight || 0) -
-			(outerContainer.current?.clientHeight || 0);
-		const newX = x + deltaX;
-
-		if (newX === oldDeltaH) {
-			setTimeout(() => {
-				const deltaH =
-					(innerContainer.current?.scrollHeight || 0) -
-					(outerContainer.current?.clientHeight || 0);
-				setX(cap(0, deltaH, deltaH));
-			}, 100);
-		} else {
-			setX(cap(0, newX, deltaH));
-		}
-		setOldDeltaH(deltaH);
-		setDeltaX(0);
-	});
-
-	useLayoutEffect(() => {
-		setTimeout(() => {
-			const deltaH =
-				(innerContainer.current?.scrollHeight || 0) -
-				(outerContainer.current?.clientHeight || 0);
-			setOldDeltaH(deltaH);
-			setX(cap(0, deltaH, deltaH));
-		}, 100);
-	}, [innerContainer, outerContainer]);
-
-	const scrollToBottom = () => {
-		const deltaH =
-			(innerContainer.current?.scrollHeight || 0) -
-			(outerContainer.current?.clientHeight || 0);
-		setDeltaX(0);
-		setX(deltaH);
-		setOldDeltaH(deltaH);
-	};
-
-	useLayoutEffect(() => {
-		setTimeout(() => scrollToBottom(), 500);
-	}, []);
+export default function ScrollContainer(
+	props: ScrollContainerProps
+): JSX.Element {
+	const {
+		x,
+		isScrolledToBottom,
+		innerContainerRef,
+		outerContainerRef,
+		scrollDelta,
+		scrollToBottom
+	} = useCustomScrollController();
 
 	return (
 		<div
 			className="scroll-container"
-			ref={outerContainer}
+			ref={outerContainerRef}
 			onWheel={(e) => scrollDelta(e.deltaY)}>
 			<div
 				className="scroll-container-inner"
-				ref={innerContainer}
+				ref={innerContainerRef}
 				style={{ top: `${-x}px` }}>
-				{children}
+				{props.children}
 			</div>
 
-			{x !== oldDeltaH && (
+			{!isScrolledToBottom && (
 				<button uxp-variant={'cta'} onClick={scrollToBottom}>
 					Scroll to bottom
 				</button>
