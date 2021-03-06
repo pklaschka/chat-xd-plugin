62d25863888be597b0c8087abee263945a31bf95
Pablo Klaschka <contact@pabloklaschka.de>
2021-03-06 15:52:09 +0100

    chore: Enhance ESLint configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,15 +5,44 @@ module.exports = {
 		es6: true,
 		node: true
 	},
-	extends: 'eslint:recommended',
+	extends: [
+		'prettier',
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:jsdoc/recommended'
+	],
+	parser: '@typescript-eslint/parser',
 	parserOptions: {
 		ecmaVersion: 2017
 	},
-	plugins: ['prettier'],
+	plugins: ['prettier', '@typescript-eslint', 'eslint-plugin-tsdoc', 'jsdoc'],
 	rules: {
-		'prettier/prettier': 'error',
 		'no-unused-vars': 0,
 		'no-console': 'off',
-		'no-undef': 'off'
+		'no-undef': 'off',
+		'tsdoc/syntax': 'error',
+		'prettier/prettier': 'warn',
+		'@typescript-eslint/no-inferrable-types': 0,
+		'jsdoc/check-tag-names': 0, // done by tsdoc checker
+		'jsdoc/require-jsdoc': [
+			'error',
+			{
+				contexts: [
+					'TSInterfaceDeclaration',
+					'TSTypeAliasDeclaration',
+					'TSEnumDeclaration'
+				]
+			}
+		],
+		'jsdoc/require-description': 1,
+		'jsdoc/require-param-description': 'error',
+		'jsdoc/require-hyphen-before-param-description': 'error',
+		'jsdoc/no-types': 'error',
+		'jsdoc/require-throws': 'error',
+		'jsdoc/require-param-type': 0,
+		'jsdoc/require-property-type': 0,
+		'jsdoc/require-returns-type': 0,
+		'jsdoc/require-example': 2,
+		'@typescript-eslint/no-explicit-any': 0
 	}
 };
